7e83b827500b79657f2b339613146195e119fe3d
arifraj199 <karif9514@gmail.com>
2022-05-23 16:47:48 +0600

    get all data from server and get on data by filtering id api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,8 +9,39 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.otsqa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const componentCollection = client.db("computerComponent").collection("component");
+
+        app.get('/component',async(req,res)=>{
+            const query = {};
+            const result = await componentCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/purchase/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await componentCollection.findOne(query);
+            res.send(result);
+        })
+    }
+    finally{}
+}
+
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Computer Component server is running on UI')
 })
 
 app.listen(port, () => {
