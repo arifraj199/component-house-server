37a4bd6adcdbd97f5bae99bc8ccd203fbef372e8
arifraj199 <karif9514@gmail.com>
2022-05-26 19:26:21 +0600

    component delete api added

--- index.js
+++ index.js
@@ -292,6 +292,13 @@ async function run() {
       const result = await userCollection.deleteOne(query);
       res.send(result);
     });
+
+    app.delete("/component/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await componentCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
